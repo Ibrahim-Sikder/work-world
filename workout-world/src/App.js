f8cccd4fa5973e341fa5fb01c735f7e507616abe
freelancer-ibrahim <ibrahimfr4450@gmail.com>
2022-09-29 18:52:27 +0600

    my-personal-info-added

--- workout-world/src/App.js
+++ workout-world/src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import Home from './components/Home/Home';
+import Question from './components/Question/Question';
 
 
 function App() {
   return (
     <div>
       <Home></Home>
+      <Question></Question>
     </div>
   );
 }
